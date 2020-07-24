7bb26d1d1e292ee69a6f92579102642645524a33
mehulraj19 <mehul.raj2018@vitstudent.ac.in>
2020-07-24 14:28:15 +0530

    Sign-Up Page

---
+++ app.js
@@ -0,0 +1,65 @@
+
+const express = require("express");
+const bodyParser  = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var email = req.body.email;
+
+  var data = {
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: fname,
+          LNAME: lname
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+  const url = "https://us10.api.mailchimp.com/3.0/lists/listidhere";
+  const options={
+    method: "POST",
+    auth:""//enter you api key here
+  };
+
+  const request = https.request(url,options,function(response){
+
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html")
+    }else{
+      res.sendFile(__dirname + "/failure.html")
+    }
+
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+console.log("server is running on port: 3000");
+
+})
